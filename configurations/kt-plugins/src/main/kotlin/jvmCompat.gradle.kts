ed3c5e68714f20ad0ac2a3437dedbfddb6c04190
Kevin Le Perf <codlabtech@gmail.com>
2025-09-13 00:28:04 +0200

    proper jvmCompat

--- configurations/kt-plugins/src/main/kotlin/jvmCompat.gradle.kts
+++ configurations/kt-plugins/src/main/kotlin/jvmCompat.gradle.kts
@@ -1,21 +1,22 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val javaVersion: String = if (rootProject.extra.has("javaVersion")) {
+val javaVersion: JvmTarget = if (rootProject.extra.has("javaVersion")) {
     val version = rootProject.extra.get("javaVersion") as String
     println("using $version")
-    version
+    JvmTarget.fromTarget(version)
 } else {
     println("Warning : you didn't define any javaVersion in your rootProject. Using 17 by default")
-    "17"
+    JvmTarget.JVM_17
 }
 
 tasks.withType<KotlinCompile>().configureEach {
-    kotlinOptions {
-        jvmTarget = javaVersion
+    compilerOptions {
+        jvmTarget.set(javaVersion)
     }
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    sourceCompatibility = javaVersion
-    targetCompatibility = javaVersion
+    sourceCompatibility = javaVersion.target
+    targetCompatibility = javaVersion.target
 }
